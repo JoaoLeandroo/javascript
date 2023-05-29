10473b3e542769f7a80c4ec8cf609440ff00b53b
joaoleandro <new.joaoleandro@gmail.com>
2023-05-29 13:42:12 -0300

    mexendo com objetos no js

---
+++ objetos/objecto.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Luffy',
+    job: 'Pirate',
+    anime: 'One Piece'
+}
+
+// essa é a forma de acessar um valor dentro de um objeto
+console.log(`Nome do Personagem ${person.name}`)
+console.log(`o personagem ${person.name} é o protagonista do anime ${person.anime}`)
