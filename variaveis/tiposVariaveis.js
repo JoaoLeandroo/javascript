0bf913c87470f55411ac9dbfba89c5b0ea581120
joaoleandro <new.joaoleandro@gmail.com>
2023-05-29 13:03:09 -0300

    segundo commit

--- variaveis/tiposVariaveis.js
+++ variaveis/tiposVariaveis.js
@@ -1,8 +1,15 @@
 // o var é uma variavel de escopo global, não possui escopo de bloco, não é recomendado o seu uso.
 var a = 'escopo global'
+var num1 = 15
+var verdadeiro = true
 
 // o let tem escopo de bloco, o que significa que ela é acessível apenas dentro do bloco onde é declarada.
 let b = 'escopo de bloco'
+let num2 = 15
+let falso = false
 
-// 
+
+// o const é uma constante, ou seja, seu valor não pode ser alterado depois de atribuído
 const c = 'valor fixo'
+const num3 = 15
+const booleano = true
