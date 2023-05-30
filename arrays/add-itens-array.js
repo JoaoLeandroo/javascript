53c8d38013520ba1accce6453a3dac93736d31f1
joaoleandro <new.joaoleandro@gmail.com>
2023-05-30 11:16:51 -0300

    usando push no array

---
+++ arrays/add-itens-array.js
@@ -0,0 +1,9 @@
+const listaConvidados = ['Bruna', 'Leandro', 'Renata', 'Carlos', 'Lucas', 'Maria']
+
+console.log(listaConvidados)
+
+let novoConvidado = 'Gabriela'
+
+listaConvidados.push(novoConvidado)
+
+console.log(`Lista Atualizada: ${listaConvidados}`)
