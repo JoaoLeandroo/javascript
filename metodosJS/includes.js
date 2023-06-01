e1f01eadc79b16102a8261732cc26e9631a51493
joaoleandro <new.joaoleandro@gmail.com>
2023-06-01 16:17:36 -0300

    adicionado o metodo includes

---
+++ metodosJS/includes.js
@@ -0,0 +1,4 @@
+const listaConvidados = ['joao', 'rafaela', 'leticia', 'carla', 'lucas', 'rafael']
+
+// o metodo includes faz uma verificação se existe na lista, se existir retorna true
+console.log(listaConvidados.includes('rafaela'))
