173f043bf51c7049deb7053548ea6ae04581729e
joaoleandro <new.joaoleandro@gmail.com>
2023-06-03 13:27:10 -0300

    table

--- extras/consoleTable.js
+++ extras/consoleTable.js
@@ -1,8 +1,28 @@
-const person = {
-    name: 'Leandro',
-    age: 24,
-    city: 'Recife',
-    job: 'Programmer'
-}
+// const person = {
+//     name: 'Leandro',
+//     age: 24,
+//     city: 'Recife',
+//     job: 'Programmer'
+// }
 
-console.table(person)
+// console.table()
+
+const cars = [
+    {
+        model: 'Ferrari',
+        year: '2022'
+    },
+    {
+        model: 'Mercedes',
+        year: '2018'
+    },
+    {
+        model: 'Porsche',
+        year: '2020'
+    },
+    {
+        model: 'Renoult',
+        year: '2019'
+    },
+]
+console.table(cars)
