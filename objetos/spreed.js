123b5ec04f55f7f72c591b097f6f8c318c38a626
joaoleandro <new.joaoleandro@gmail.com>
2023-06-11 19:15:47 -0300

    operador spreed

---
+++ objetos/spreed.js
@@ -0,0 +1,6 @@
+
+const lista = ['Palmeiras', 'Flamengo', 'Cruzeiro', 'Santos', 'Gremio']
+const lista2 = ['Barcelona', 'Real Madrid', 'Juventus', 'Manchester United', 'Bayern de Munique']
+const copia = [...lista, ...lista2]
+
+console.log(copia)
