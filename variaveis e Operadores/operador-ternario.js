88b36107be683c9de5bf443259e67110f24e8540
joaoleandro <new.joaoleandro@gmail.com>
2023-06-17 19:26:56 -0300

    ternario

--- variaveis e Operadores/operador-ternario.js
+++ variaveis e Operadores/operador-ternario.js
@@ -2,3 +2,11 @@
 const maior = 4 > 12 ? 'O primeiro Numero é maior' : 'O segundo numero é maior'
 
 console.log(maior)
+
+const texto = ""
+const formattedText = texto ? texto.replace("a", "b") : ''
+console.log(formattedText)
+
+const texto2 = "aaaaaaaaaa"
+const formattedText2 = texto2 ? texto2.replaceAll("a", "b") : ''
+console.log(formattedText2)
