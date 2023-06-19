68644dbed254291153dc7741e3e45ad694cc26c2
joaoleandro <new.joaoleandro@gmail.com>
2023-06-19 17:52:57 -0300

    metodo split

---
+++ extras/split.js
@@ -0,0 +1,11 @@
+// const data = "2023-06-19"
+// const partes = data.split("-")
+// console.log(partes)
+
+// const texto = "Times do Futebol Brasileiro, Palmeiras, Flamengo, Cruzeiro, Corinthians, São Paulo, Santos, Atletico-mg, America-mg"
+// const newText = texto.split(", ")
+// console.log(newText)
+
+const limit = "Palmeiras, Flamengo, Cruzeiro, Corinthians, São Paulo"
+const newLimit = limit.split(", ", 2)
+console.log(newLimit)
