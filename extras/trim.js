12cb47280729ce7c58ec6270974661a12fbe03d3
joaoleandro <new.joaoleandro@gmail.com>
2023-07-11 14:18:45 -0300

    trimEnd

--- extras/trim.js
+++ extras/trim.js
@@ -1,4 +1,10 @@
 const str = '  hello'
 const trimStr = str.trimStart()
 console.log(trimStr)
-// o trimStart remove os espaços da string
+// o trimStart remove os espaços da string no inicio
+
+
+
+const newTest = '  hello  '
+console.log(newTest.trimEnd())
+// o trim end, remove os espaços no final da string
