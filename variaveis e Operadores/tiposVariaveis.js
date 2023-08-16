b475726e4910682e0a1459a52ad67fdfbdfa9edf
joaoleandro <new.joaoleandro@gmail.com>
2023-08-16 08:24:08 -0300

    var

--- variaveis e Operadores/tiposVariaveis.js
+++ variaveis e Operadores/tiposVariaveis.js
@@ -1,5 +1,6 @@
 // o var é uma variavel de escopo global, não possui escopo de bloco, não é recomendado o seu uso.
 var a = 'escopo global'
+var a = 'uma variavel pode ser declarada com o mesmo nome, isso é um problema.'
 var num1 = 15
 var verdadeiro = true
 
