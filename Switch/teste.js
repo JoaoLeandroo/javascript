b36675f550e4b11b552b6adf2fc779ffd0b94f4d
joaoleandro <new.joaoleandro@gmail.com>
2023-08-27 13:56:40 -0300

    teste

---
+++ Switch/teste.js
@@ -0,0 +1,14 @@
+// const test = 10
+
+// switch(test) {
+//     case 10: 
+//         console.log("sucesso")
+//     break;
+//     case 0: 
+//         console.log("Falha")
+//     break;
+//     default:
+//         console.log("Número Incorreto.")
+//     break;
+// }
+
