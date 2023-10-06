064dec7ee3696f519cb6e86f33eb1d17d2fcf7ff
joaoleandro <new.joaoleandro@gmail.com>
2023-10-06 09:15:29 -0300

    matrix

---
+++ arrays/matrix.js
@@ -0,0 +1,11 @@
+let mochila = []
+
+let item1 = ["Corda", 2]
+let item2 = ["Frutas", 3]
+let item3 = ["Remedios", 5]
+let item4 = ["Lanterna", 1]
+
+
+mochila.push(item1, item2, item3, item4)
+console.log(mochila)
+console.log(mochila[2])
