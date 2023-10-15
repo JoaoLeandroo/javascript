5ce6b71108ae532911de7d2d963eea72feb9dbba
joaoleandro <new.joaoleandro@gmail.com>
2023-10-15 10:01:30 -0300

    table

---
+++ extras/table.js
@@ -0,0 +1,24 @@
+const shop = [
+    {
+        item: "Notebook",
+        valor: 3.849,
+        model: "Core i5"
+    },
+    {
+        item: "Ventilador",
+        valor: 299.99,
+        model: "Arnor"
+    },
+    {
+        item: "Tv",
+        valor: 2.900,
+        model: "LG"
+    },
+    {
+        item: "Liquidificador",
+        valor: 245.90,
+        model: "Arnor"
+    }
+]
+
+console.table(shop)
