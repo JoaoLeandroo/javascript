96f5ceb28613d5138e5bd6cdce4e54a9cf5e97e5
joaoleandro <new.joaoleandro@gmail.com>
2023-12-12 20:41:23 -0300

    melhorando a requisição da api

---
+++ api/melhorando.js
@@ -0,0 +1,14 @@
+const URL_API_TESTE = "https://rickandmortyapi.com/api/character";
+
+async function loadApi(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+    }
+    catch(error) {
+        console.log("ERRO AO CARREGAR API " + error)
+    }
+}
+
+loadApi(URL_API_TESTE);
