24919eddb9526375e8605a74678f46cd970538d3
joaoleandro <new.joaoleandro@gmail.com>
2024-01-09 00:53:00 -0300

    metodo array, splice

---
+++ arrays/splice.js
@@ -0,0 +1,7 @@
+let frutas = ['banana', 'uva', 'morango', 'melancia', 'abacate', 'maracuja']
+
+frutas.splice(3, 1)
+// Remove os itens do array de acordo com o index, o primeiro parametro do splice é o index do elemento
+// o segundo parametro é quantos elementos devem ser removidos a partir dele.
+
+console.log(frutas)
