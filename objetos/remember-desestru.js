fe48e6dc1a0465d7a0dbb6631465e1b54e1ee77b
joaoleandro <new.joaoleandro@gmail.com>
2024-01-13 08:39:52 -0300

    destructuring remember

---
+++ objetos/remember-desestru.js
@@ -0,0 +1,28 @@
+const personsOnePiece = {
+    nome: [
+        "luffy",
+        "zoro",
+        "sanji",
+        "nami",
+        "usop",
+        "robin",
+        "vivi",
+        "franky"
+    ],
+    cargo: [
+        "capitão",
+        "primeiro imediato",
+        "cozinheiro",
+        "Navegadora",
+        "Mentiroso",
+        "Arqueologa",
+        "princesa",
+        "Carpintero"
+    ]
+}
+
+const [primeiro, segundo, terceiro] = personsOnePiece.nome
+
+console.log(primeiro)
+console.log(segundo)
+console.log(terceiro)
