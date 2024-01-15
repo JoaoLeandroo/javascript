325077aef45edf82dcaab7a2bc817b4b70289389
joaoleandro <new.joaoleandro@gmail.com>
2024-01-15 07:30:34 -0300

    map

---
+++ arrays/map.js
@@ -0,0 +1,10 @@
+const testing = ['force', 'ssss', 'black', 'yellow', 'testing']
+
+function x(arr) {
+    let tes = arr
+    tes.map((itens) => {
+        console.log(itens)
+    })
+}
+
+x(testing)
