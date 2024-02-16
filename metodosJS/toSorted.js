54738dd89f1ef8daca02b0012979082cb797615d
joaoleandro <new.joaoleandro@gmail.com>
2024-02-16 14:36:18 -0300

    toSorted

---
+++ metodosJS/toSorted.js
@@ -0,0 +1,8 @@
+
+// Com o metodo toSorted o array original, não sofre alterações
+const arr = [5, 8, 9, 4, 2, 3]
+console.log(arr)
+console.log("-------------------------")
+const sorted = arr.toSorted()
+console.log(sorted)
+
