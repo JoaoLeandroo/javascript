a372866fe546b071c74dbe1a2221ac1c29f58f22
joaoleandro <new.joaoleandro@gmail.com>
2024-02-18 15:09:32 -0300

    isInteger

---
+++ metodosJS/isInteger.js
@@ -0,0 +1,5 @@
+// O metodo isInteger verifica de um número é do tipo Inteiro, retornando true or false
+const n1 = 12
+
+// se o retorno for igual true, o número é inteiro
+console.log(Number.isInteger(n1))
