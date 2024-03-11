6fbcea45f41c2b0aa519bb4c9ca882fd3f198ed0
joaoleandro <new.joaoleandro@gmail.com>
2024-03-11 09:42:03 -0300

    IndexOf

---
+++ extras/indexOf.js
@@ -0,0 +1,8 @@
+lista = ["Maça", "teste", "Pêra", "Banana"]
+
+const x = lista.indexOf("Banana")
+if(x != -1) {
+    console.log("Elemento encontrado na posição " + x)
+}else{
+    console.log("Nao encontrado")
+}
