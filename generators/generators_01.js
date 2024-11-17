7b6579de18bfcbd9831bb3718dcb37530ea96457
joaoleandro <new.joaoleandro@gmail.com>
2024-11-17 15:21:16 -0300

    metodo generators

---
+++ generators/generators_01.js
@@ -0,0 +1,12 @@
+
+// * transforma a function em um generator
+function* testeGenerator() {
+    console.log("Teste 1")
+    yield console.log("Teste 2") //metodo yild vai executar até essa linha
+    console.log("Teste 3")
+}
+
+const teste = testeGenerator()
+
+// next chama a função para ser executada
+teste.next()
